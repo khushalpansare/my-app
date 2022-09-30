0e877bfd95570b261057d92220f3b5bd450aa17d
Khushpansare <hkhushalpansare@gmail.com>
2022-09-30 18:37:28 +0530

    about url

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React from "react";
 import RegisterPage from "../src/Pages/RegisterPage";
 import LoginPage from "../src/Pages/LoginPage";
 import Listpage from "../src/Pages/Listpage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Navigate,
-  useRoutes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./Component/Profile";
 import NavigationBar from "./Component/NavigationBar";
 
@@ -17,8 +10,8 @@ function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<RegisterPage />} />
+        <Routes basename="/my-app">
+          <Route path="/my-app" element={<RegisterPage />} />
           <Route exact path="/loginpage" element={<LoginPage />} />
           <Route
             exact
